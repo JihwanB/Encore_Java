3f5c81276cb7deff5f1401279a9b3d3f4d2cc345
JihwanB <leyqorwlghks@gmail.com>
2024-02-14 15:45:45 +0900

    Created package: '/com/encore/collection/list

---
+++ src/com/encore/collection/list/ArrayListMain.java
@@ -0,0 +1,147 @@
+package com.encore.collection.list;
+
+import com.encore.hms.domain.EmployeeDTO;
+import com.encore.hms.domain.StudentDTO;
+import com.encore.hms.domain.TeacherDTO;
+import com.encore.hms.domain.sup.Person;
+
+import java.util.*;
+
+public class ArrayListMain {
+
+    public static void main(String[] args) {
+
+        /*
+        Number[] ary = new Number[5]; // static allocation
+        for (int i = 0; i < 5; i++) {
+            ary[i] = 10 * (i + 1);
+        }
+        System.out.println("length : " + ary.length);
+        ary[2] = null;
+        System.out.println(Arrays.toString(ary));
+         */
+
+        /*
+        List<Number> lst = new ArrayList<>();
+        for (int i = 1; i <= 5; i++) {
+            lst.add(i * 10);
+        }
+        System.out.println("length : " + lst.size());
+        lst.remove(2);
+        System.out.println("length : " + lst.size());
+         */
+
+        List<String> lst01 = new ArrayList<>();
+        lst01.add("A");
+        lst01.add("B");
+        lst01.add("C");
+        lst01.add("D");
+        lst01.add("E");
+        System.out.println("length : " + lst01.size());
+
+        List<String> lst02 = new ArrayList<>();
+        lst02.add("1");
+        lst02.add("2");
+        lst01.addAll(lst02);
+        System.out.println(lst01);
+        lst01.add(3, "F");
+        System.out.println(lst01);
+        // 주의점 : 중간에 빈공간 (null) 을 허용하지 않음
+        // null 을 포함하는 인덱스 번지에 추가하는 것은 예외가 발생됨!!!
+        // lst01.add(9, "F"); -- exception
+
+        // 특정 인덱스 번지의 데이터를 삭제
+        lst01.remove(0);
+        System.out.println(lst01);
+
+        // 요소 검색
+        System.out.println(lst01.contains("C") ? "요소검색 성공" : "요소검색 실패");
+        System.out.println("검색된 요소의 인덱스 번지 : " + lst01.indexOf("C"));
+
+        // 요소를 추출
+        System.out.println(lst01.get(0));
+
+        // 요소의 부분 추출
+        List<String> subLst = lst01.subList(0, 5); // [start, end)
+        System.out.println(subLst);
+
+        // 요소 변경
+        lst01.set(1, "encore");
+        System.out.println(lst01);
+
+        // 사이즈를 동적으로 늘리는 작업
+        ((ArrayList<String>) lst01).ensureCapacity(100);
+        System.out.println(lst01.size());
+
+        // 보안상의 이유로 Deep copy 를 해야한다면?
+        List<String> copyLst = (List<String>) ((ArrayList<String>) lst01).clone();
+        System.out.println("original : " + lst01);
+        System.out.println("copy : " + copyLst);
+        lst01.set(0, "encore");
+        System.out.println("original : " + lst01);
+        System.out.println("copy : " + copyLst);
+
+        // 자료구조를 배열로 반환해야 한다면?
+        Object[] ary = copyLst.toArray();
+        System.out.println(Arrays.toString(ary));
+
+        // 정렬
+        List<Integer> sortLst = new ArrayList<>();
+        sortLst.add(3);
+        sortLst.add(2);
+        sortLst.add(1);
+        System.out.println(sortLst);
+        System.out.println(" --- sort --- ");
+        sortLst.sort(Comparator.naturalOrder());
+        System.out.println("ascending - " + sortLst);
+        sortLst.sort(Comparator.reverseOrder());
+        System.out.println("descending - " + sortLst);
+
+        // 순회
+        List<Person> hmsLst = new ArrayList<>();
+        hmsLst.add(new StudentDTO("임정섭", 20, "서울", "1992"));
+        hmsLst.add(new TeacherDTO("임정섭", 20, "서울", "자바"));
+        hmsLst.add(new EmployeeDTO("임정섭", 20, "서울", "교육 서비스"));
+
+        /*
+        for (int i = 0; i < hmsLst.size(); i++) {
+            Person obj = hmsLst.get(i);
+            System.out.println(obj.personInfo());
+        }
+         */
+        // Iterator
+        Iterator<Person> it = hmsLst.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next().personInfo());
+        }
+
+        System.out.println("다시 한번 순회");
+        it = hmsLst.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next().personInfo());
+        }
+
+        // List 에만 있는 ListIterator()
+        ListIterator<Person> iter = hmsLst.listIterator();
+        while (iter.hasNext()) {
+            System.out.println(iter.next().personInfo());
+        }
+        while (iter.hasPrevious()) {
+            System.out.println(iter.previous().personInfo());
+        }
+
+        // 전체 데이터를 삭제
+        lst01.clear();
+        System.out.println(lst01);
+
+        // 모든 Collection 은 객체를 담는 자료구조(그릇)
+        List<Integer> numberLst = new ArrayList<>();
+        // int - Integer, char - Character (Wrapper Class)
+        // int x = 10; Integer xx = new Integer(x); -> auto boxing
+        // Integer xx = new Integer(10); int z = (int) xx + 10; -> auto unboxing
+        // numberLst.add('A');
+        numberLst.add(10);
+
+    }
+
+}
---
+++ src/com/encore/collection/list/ListDemoMain.java
@@ -0,0 +1,42 @@
+package com.encore.collection.list;
+
+import com.encore.hms.domain.StudentDTO;
+import com.encore.hms.domain.TeacherDTO;
+import com.encore.hms.domain.sup.Person;
+
+import java.util.*;
+
+/*
+ List 걔열의 자료구조는 순서가 있고(내부적으로 인덱스를 0부터 관리), 중복을 허용, 객체를 담으며(Object 상속하는 타입), 동적이다.
+ Set 계열의 자료구조는 순서가 없고, 중복을 허용하지 않음, 객체를 담으며(Object 상속하는 타입), 동적이다.
+ */
+public class ListDemoMain {
+
+    public static void main(String[] args) {
+
+        List<Person> col01 = new ArrayList<>();
+        //col01.add("A");
+        //col01.add("B");
+        // col01.add(1); -- error 제네릭스 문법을 적용하면 타입의 안정성 체크로 컴파일시 오류
+
+        col01.add(new StudentDTO());
+        col01.add(new StudentDTO());
+        col01.add(new TeacherDTO());
+
+        System.out.println("배열의 길이는 length, 자료구조는 size() : " + col01.size());
+        System.out.println("데이터를 가져온다면 : " + col01.get(0) + "\t" + col01.get(1));
+        System.out.println("제네릭스 문법을 적용하지 않을 때 반환되는 타입은 Object ");
+        System.out.println("런타임 시점에 객체에 접근하기 위해서는 캐스팅이 필요 ");
+        System.out.println("데이터를 가져온다면 : " + "\n\t" + ((StudentDTO) col01.get(0)).personInfo() + "\n"
+                + col01.get(1).personInfo() + "\n" + col01.get(2).personInfo());
+        System.out.println();
+
+        List<String> col02 = new LinkedList<>();
+        col02.add("B");
+        col02.add("c");
+        col02.add("D");
+        System.out.println(col02.get(0) + "\t" + col02.get(1) + "\t" + col02.get(2));
+
+    }
+
+}

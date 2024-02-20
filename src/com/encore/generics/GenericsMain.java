d444aae664b8539255a557132a6b1df34fb1b23f
JihwanB <leyqorwlghks@gmail.com>
2024-02-20 15:06:09 +0900

    Added examples regarding generics

--- src/com/encore/generics/GenericsMain.java
+++ src/com/encore/generics/GenericsMain.java
@@ -41,14 +41,43 @@ public class GenericsMain {
 
         // 제너릭스의 다형성
         // 와일드카드(?)를 사용할 수 있다.
-        // <? extends Skating> : Skating 을 상속받는 모든 하위 객체르 포함하겠다는 의미
-        // <? super Golf> : Golf 의 부모
+        // <? extends Skating> : Skating 을 상속받는 모든 하위 객체를 포함하겠다는 의미
+        // <? super Golf> : Golf 와 상위 객체타입인 Object
         // extends 는 '?' 보다 하위를 포함하고, super 는 '?'의 부모 타입을 포함
-        List<? super Sports> lst = new ArrayList<>();
-        lst.add(new Golf());
-        lst.add(new Skating());
-        lst.add(new SpeedSkating());
+
+        // List<?> 저장은 X, 꺼낼 수 있는데 반환 타입은 Object
+        // 메서드의 리턴타입, 매개변수 지정시 사용
+        /*
+        List<?> lst = new ArrayList<>();
+        lst.add(null); // 널값은 허용
+        lst.add(new Golf()); // 객체를 담을 수 없기 때문에 error
+         */
+
+        // List<? extends Skating> == List<Skating>
+        // 저장은 X, 꺼낼 수 있는데 반환 타입은 Skating
+        // 메서드의 리턴타입, 매개변수 지정시 사용
+        /*
+        List<? extends Skating> lst = new ArrayList<>();
+        lst.add(new SpeedSkating());    // 객체를 담을 수 없기 때문에 error
+        lst.add(null);
+         */
+
+        // 저장 가능(타입 : Golf, 상위 타입)
+        // 반환타입은 Object
+        // List<? super Golf> lst = new ArrayList<>();
+
+        //List<Sports> lst = new ArrayList<>();
+        //lst.add(new Golf());
+        //lst.add(new Skating());
+
+        List<Skating> lst = new ArrayList<>();
         lst.add(new ShortTrackSkating());
+        lst.add(new SpeedSkating());
+        getExtendsMethod(lst);
+
+    }
+
+    public static void getExtendsMethod(List<? super Skating> lst) {
 
     }
 
